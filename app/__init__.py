204e2cf74b09d4519e87f173c6a57625bfca14f5
julia-castro <julia.castro@outlook.com>
2017-03-22 17:31:50 -0400

    specify configurations for different environments

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
 
-app = Flask(__name__, instance_relative_config=True)
+db = SQLAlchemy()
 
-from app import views
+def create_app(config_name):
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_object(app_config[config_name])
+  app.config.from_pyfile('config.py')
+  db.init_app(app)
 
-app.config.from_object('config')
+  # temporary route
+  @app.route('/')
+  def hello_world():
+      return 'Hello, World!'
+
+  return app
--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-DEBUG = True
+class Config(object):
+  print('config object')
+  #put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+  DEBUG = True
+  SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+  DEBUG = False
+
+
+app_config = {
+  'development': DevelopmentConfig,
+  'production': ProductionConfig
+}
